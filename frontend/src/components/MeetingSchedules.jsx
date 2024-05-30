e07cbc98ca9c8913bdd72819918312a453438122
Madhav-Sarma <a.madhavsarma@gmail.com>
2024-05-30 23:51:25 +0530

    meeting schedule temp

--- frontend/src/components/MeetingSchedules.jsx
+++ frontend/src/components/MeetingSchedules.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Layout from './Layout';
 import MenteeRemarkCard from './MenteeRemarkCard';
+import { Link } from 'react-router-dom';
 function MeetingSchedules() {
   return (
     <Layout>
       <div className="">
-        <div className="upcomingmeetings"></div>
+        <div className="upcomingmeetings">
+          <h1>upcomingmeetings</h1>
+        <MenteeRemarkCard />
+        </div>
         <hr/>
         <div className="pastmeetings">
-          <MenteeRemarkCard>hi</MenteeRemarkCard>
+          <h1>past meetings</h1>
+          <Link to='/'><MenteeRemarkCard /></Link>
         </div>
       </div>
     </Layout>
