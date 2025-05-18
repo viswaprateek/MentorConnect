1135bedc54c800abb1d7b21cc9f9154a2fb772d2
viswaprateek248 <viswaprateek248@gmail.com>
2025-05-18 12:21:16 +0530

    base url changed

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -8,7 +8,8 @@ import axios from 'axios';
 
 export const api = axios.create({
   // baseURL: 'http://localhost:4000', // The base URL for API requests
-  baseURL: 'http://3.83.224.243:5000', // The base URL for API requests
+  // baseURL: 'http://18.60.146.60:5000', // The base URL for API requests
+  baseURL: 'https://mentorconnectbackend-h9w6wggwy-turinghut-temp.vercel.app/',
   // withCredentials:true,
   headers: {
     'Content-Type': 'application/json', // Common content type header for JSON data
